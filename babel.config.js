4d5ed1442d0e013a3a7da85c2b944c3fbf485c90
Tanish garg <tanishg566@outlook.com>
2022-07-20 12:33:10 +0530

    Fixed ENV Staging

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,18 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['module:metro-react-native-babel-preset',
+    'module:react-native-dotenv'],
     plugins: [
       ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
         "moduleName": "@env",
         "path": ".env",
         "blocklist": null,
         "allowlist": null,
         "safe": false,
         "allowUndefined": true
-      }],
+      }]
     ]
   };
 };
