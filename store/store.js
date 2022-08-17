c80898d9ed6620a45aa0b73d68181820b1f19674
Tanish garg <tanishg566@outlook.com>
2022-08-17 15:42:19 +0530

    added

--- store/store.js
+++ store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-community/async-storage";
 import { persistStore, persistReducer } from "redux-persist";
 
 import aadhaarSlice from "./slices/aadhaarSlice";
