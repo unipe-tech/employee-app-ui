0043373fe2f7bc21a94133a53156494fe7c5657f
Tanish garg <tanishg566@outlook.com>
2022-10-06 10:26:05 +0530

    change

---
+++ SomeTaskName.js
@@ -0,0 +1,5 @@
+module.exports = async () => {
+  setTimeout(() => {
+    console.log("this world is synking");
+  }, 1000);
+};
--- index.js
+++ index.js
@@ -7,3 +7,5 @@ import App from "./App";
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+import { AppRegistry } from "react-native";
+AppRegistry.registerHeadlessTask("SomeTaskName", () => SomeTaskName);
