576820545fe6f608b103955d29acce35e5f5f24f
Tanish garg <tanishg566@outlook.com>
2022-10-28 09:59:18 +0530

    change

--- apis/pan/Verify.jsx
+++ apis/pan/Verify.jsx
@@ -31,37 +31,37 @@ const PanVerifyApi = (props) => {
     panSlice?.verifyTimestamp
   );
 
-  // useEffect(() => {
-  //   dispatch(addData(data));
-  // }, [data]);
+  useEffect(() => {
+    dispatch(addData(data));
+  }, [data]);
 
-  // useEffect(() => {
-  //   dispatch(addVerifyMsg(verifyMsg));
-  // }, [verifyMsg]);
+  useEffect(() => {
+    dispatch(addVerifyMsg(verifyMsg));
+  }, [verifyMsg]);
 
-  // useEffect(() => {
-  //   dispatch(addVerifyStatus(verifyStatus));
-  // }, [verifyStatus]);
+  useEffect(() => {
+    dispatch(addVerifyStatus(verifyStatus));
+  }, [verifyStatus]);
 
-  // useEffect(() => {
-  //   dispatch(addVerifyTimestamp(verifyTimestamp));
-  // }, [verifyTimestamp]);
+  useEffect(() => {
+    dispatch(addVerifyTimestamp(verifyTimestamp));
+  }, [verifyTimestamp]);
 
-  // useEffect(() => {
-  //   console.log("PanVerifyApi backendPush panSlice: ", backendPush, panSlice);
-  //   if (backendPush) {
-  //     panBackendPush({
-  //       id: id,
-  //       data: data,
-  //       number: panSlice?.number,
-  //       verifyMsg: verifyMsg,
-  //       verifyStatus: verifyStatus,
-  //       verifyTimestamp: verifyTimestamp,
-  //     });
-  //     setBackendPush(false);
-  //   }
-  //   setLoading(false);
-  // }, [backendPush]);
+  useEffect(() => {
+    console.log("PanVerifyApi backendPush panSlice: ", backendPush, panSlice);
+    if (backendPush) {
+      panBackendPush({
+        id: id,
+        data: data,
+        number: panSlice?.number,
+        verifyMsg: verifyMsg,
+        verifyStatus: verifyStatus,
+        verifyTimestamp: verifyTimestamp,
+      });
+      setBackendPush(false);
+    }
+    setLoading(false);
+  }, [backendPush]);
 
   var { status, ...query } = useQuery(["panFetch",props.data], () => {
     const options = {
@@ -96,76 +96,76 @@ const PanVerifyApi = (props) => {
 
   const goForFetch = () => {
     var responseJson = query.data;
-    console.log(responseJson)
-    // try {
-    //   if (responseJson["status"] == "200") {
-    //     switch (responseJson["data"]["code"]) {
-    //       case "1000":
-    //         var names = ["first", "middle", "last"];
-    //         responseJson["data"]["pan_data"]["name"] = names
-    //           .filter((k) => responseJson["data"]["pan_data"][`${k}_name`])
-    //           .map((k) => responseJson["data"]["pan_data"][`${k}_name`])
-    //           .join(" ");
-    //         console.log("PAN fetched data: ", responseJson);
-    //         setData(responseJson["data"]["pan_data"]);
-    //         setVerifyMsg("To be confirmed by User");
-    //         setVerifyStatus("PENDING");
-    //         setVerifyTimestamp(responseJson["timestamp"]);
-    //         Analytics.trackEvent("Pan|Verify|Success", {
-    //           userId: id,
-    //         });
-    //         setBackendPush(true);
-    //         {
-    //           props.type == "KYC"
-    //             ? navigation.navigate("KYC", {
-    //                 screen: "PAN",
-    //                 params: {
-    //                   screen: "Confirm",
-    //                 },
-    //               })
-    //             : navigation.navigate("PanConfirm");
-    //         }
-    //         break;
-    //       default:
-    //         setVerifyMsg(responseJson["data"]["message"]);
-    //         Analytics.trackEvent("Pan|Verify|Error", {
-    //           userId: id,
-    //           error: responseJson["data"]["message"],
-    //         });
-    //         setVerifyStatus("ERROR");
-    //         setBackendPush(true);
-    //         Alert.alert("Error", responseJson["data"]["message"]);
-    //     }
-    //   } else if (responseJson?.error?.message) {
-    //     setVerifyMsg(responseJson["error"]["message"]);
-    //     Analytics.trackEvent("Pan|Verify|Error", {
-    //       userId: id,
-    //       error: responseJson["error"]["message"],
-    //     });
-    //     setVerifyStatus("ERROR");
-    //     setBackendPush(true);
-    //     Alert.alert("Error", responseJson["error"]["message"]);
-    //   } else {
-    //     setVerifyMsg(responseJson["message"]);
-    //     Analytics.trackEvent("Pan|Verify|Error", {
-    //       userId: id,
-    //       error: responseJson["message"],
-    //     });
-    //     setVerifyStatus("ERROR");
-    //     setBackendPush(true);
-    //     Alert.alert("Error", responseJson["message"]);
-    //   }
-    //   } catch (error){
-    //     console.log("Error casda: ", error);
-    //     setVerifyMsg(error);
-    //     Analytics.trackEvent("Pan|Verify|Error", {
-    //       userId: id,
-    //       error: error,
-    //     });
-    //     setVerifyStatus("ERROR");
-    //     setBackendPush(true);
-    //     Alert.alert("Error", error);
-    //   }
+    console.log("PAN ResponseJSON: ", responseJson)
+    try {
+      if (responseJson["status"] == "200") {
+        switch (responseJson["data"]["code"]) {
+          case "1000":
+            var names = ["first", "middle", "last"];
+            responseJson["data"]["pan_data"]["name"] = names
+              .filter((k) => responseJson["data"]["pan_data"][`${k}_name`])
+              .map((k) => responseJson["data"]["pan_data"][`${k}_name`])
+              .join(" ");
+            console.log("PAN fetched data: ", responseJson);
+            setData(responseJson["data"]["pan_data"]);
+            setVerifyMsg("To be confirmed by User");
+            setVerifyStatus("PENDING");
+            setVerifyTimestamp(responseJson["timestamp"]);
+            Analytics.trackEvent("Pan|Verify|Success", {
+              userId: id,
+            });
+            setBackendPush(true);
+            {
+              props.type == "KYC"
+                ? navigation.navigate("KYC", {
+                    screen: "PAN",
+                    params: {
+                      screen: "Confirm",
+                    },
+                  })
+                : navigation.navigate("PanConfirm");
+            }
+            break;
+          default:
+            setVerifyMsg(responseJson["data"]["message"]);
+            Analytics.trackEvent("Pan|Verify|Error", {
+              userId: id,
+              error: responseJson["data"]["message"],
+            });
+            setVerifyStatus("ERROR");
+            setBackendPush(true);
+            Alert.alert("Error", responseJson["data"]["message"]);
+        }
+      } else if (responseJson?.error?.message) {
+        setVerifyMsg(responseJson["error"]["message"]);
+        Analytics.trackEvent("Pan|Verify|Error", {
+          userId: id,
+          error: responseJson["error"]["message"],
+        });
+        setVerifyStatus("ERROR");
+        setBackendPush(true);
+        Alert.alert("Error", responseJson["error"]["message"]);
+      } else {
+        setVerifyMsg(responseJson["message"]);
+        Analytics.trackEvent("Pan|Verify|Error", {
+          userId: id,
+          error: responseJson["message"],
+        });
+        setVerifyStatus("ERROR");
+        setBackendPush(true);
+        Alert.alert("Error", responseJson["message"]);
+      }
+      } catch (error){
+        console.log("Error casda: ", error);
+        setVerifyMsg(error);
+        Analytics.trackEvent("Pan|Verify|Error", {
+          userId: id,
+          error: error,
+        });
+        setVerifyStatus("ERROR");
+        setBackendPush(true);
+        Alert.alert("Error", error);
+      }
       
     }
   
