5aadf49bb20b3c5e7a0ccebbc39959368ef58322
Tanish garg <tanishg566@outlook.com>
2022-11-24 17:38:55 +0530

    initial

---
+++ tests/test/specs/App.test.js
@@ -0,0 +1,30 @@
+var expect = require("chai").expect;
+
+describe("Simple App testing", () => {
+  // Adding time out to make sure the app is load prior to test is run
+  beforeEach(() => {
+    $("~app-root").waitForDisplayed(11000, false);
+  });
+
+  it("Valid Login Test", async (async) => {
+    $("~mobile-number").setValue("9999999999");
+
+    $("~login").click();
+    expect();
+
+    // $("~loginstatus").waitForDisplayed(11000);
+    // const status = $("~loginstatus").getText();
+    // expect(status).to.equal('success');
+  });
+
+  it("Invalid Login Test", (async) => {
+    $("~mobile-number").setValue("9999999999");
+    // $('~password').setValue("12345");
+
+    $("~login").click();
+
+    // $("~loginstatus").waitForDisplayed(11000);
+    // const status = $("~loginstatus").getText();
+    // expect(status).to.equal('fail');
+  });
+});
