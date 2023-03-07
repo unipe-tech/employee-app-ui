3dedac8d6aa91036d21cd415631d1e2169bec494
Mohak Agrawal <54626046+Mohak-Agrawal@users.noreply.github.com>
2023-03-07 18:10:25 +0530

    Update KeyboardAvoidingWrapper.jsx

--- src/KeyboardAvoidingWrapper.jsx
+++ src/KeyboardAvoidingWrapper.jsx
@@ -10,19 +10,18 @@ import { styles } from "./styles";
 
 export const KeyboardAvoidingWrapper = ({ children }) => {
   return (
-    // <ScrollView
-    //   keyboardShouldPersistTaps="handled"
-    //   style={styles.container}
-    //   showsVerticalScrollIndicator={false}
-    // >
-    <KeyboardAvoidingView
-      enabled
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      showsVerticalScrollIndicator={false}
     >
-      <ScrollView showsVerticalScrollIndicator={false}>{children}</ScrollView>
-    </KeyboardAvoidingView>
-    // </ScrollView>
+      <KeyboardAvoidingView
+        enabled
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
